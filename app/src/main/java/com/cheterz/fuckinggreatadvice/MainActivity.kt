7c5ad7ff71f205c274d6d020ddab77eb04c8a7fd
che <cheterz32@gmail.com>
2020-01-16 12:23:39 +0300

    working sharing

--- app/src/main/java/com/cheterz/fuckinggreatadvice/MainActivity.kt
+++ app/src/main/java/com/cheterz/fuckinggreatadvice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.cheterz.fuckinggreatadvice
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
@@ -13,7 +14,7 @@ private const val URL = "https://fucking-great-advice.ru/api/random"
 
 class MainActivity : AppCompatActivity() {
 
-    val client = OkHttpClient()
+    private var adviceText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +30,21 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    fun run(url: String) {
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_share -> {
+                val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.type = "text/plain"
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "F***ing Great Advice:\n" + adviceText)
+                startActivity(shareIntent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun run(url: String) {
+        val client = OkHttpClient()
         val request = Request.Builder().url(url).build()
         client.newCall(request).enqueue(object : Callback {
             override fun onResponse(call: Call, response: Response) {
@@ -38,7 +53,10 @@ class MainActivity : AppCompatActivity() {
                     val gson = GsonBuilder().create()
                     val adviceFeed = gson.fromJson(advice, Advice::class.java)
                     this@MainActivity.runOnUiThread {
+                        adviceText = adviceFeed?.text ?: ""
                         this@MainActivity.tv_advice.text = adviceFeed?.text ?: ""
+
+
                     }
                 } else (this@MainActivity.runOnUiThread {
                     this@MainActivity.tv_advice.setText(R.string.advice)
